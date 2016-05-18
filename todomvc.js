d450dc618173c33b62d7ca82656c3253d6c8b5b8
Yury Shulaev <yury.shulaev@gmail.com>
2016-05-18 20:18:31 +0300

    utils.arrayRemove() as in frontend code

--- todomvc.js
+++ todomvc.js
@@ -9,7 +9,7 @@ class TodosModel {
 	constructor(todos) { this.todos = todos || []; }
 	add(title) { this.todos.push(new Todo({title: title.trim(), completed: false})); }
 	setTitle(todo, title) { (todo.title = title.trim()) || this.remove(todo); }
-	remove(todo) { view.arrayRemove(this.todos, todo); }
+	remove(todo) { utils.arrayRemove(this.todos, todo); }
 	get remaining() { return this.todos.filter(todo => !todo.completed); }
 	get completed() { return this.todos.filter(todo => todo.completed); }
 	clearCompleted() { this.todos = this.remaining; }
