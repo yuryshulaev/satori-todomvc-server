50dc20df60d4171632d755433536e9e55df68926
Yury Shulaev <yury.shulaev@gmail.com>
2016-05-18 21:39:32 +0300

    Move require('http') to the top

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const PORT = process.env.PORT || 8085;
 
+const http = require('http');
 const nodeStatic = require('node-static');
 const SatoriServer = require('satori-server').SatoriServer;
 const todomvc = require('satori-todomvc/js/todomvc');
@@ -12,7 +13,7 @@ let model = new todomvc.TodosModel(require('./data.json').map(todo => new todomv
 let vm = new todomvc.ViewModel(model, view);
 let staticServer = new nodeStatic.Server('./node_modules');
 
-require('http').createServer(function (request, response) {
+http.createServer(function (request, response) {
 	if (request.url === '/') {
 		response.end(indexHtml(todomvc.TodoAppComponent(vm, view)));
 		return;
