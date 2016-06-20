2475fc5568f785e70967fe622ecd48ccb608107c
Yury Shulaev <yury.shulaev@gmail.com>
2016-06-21 01:44:19 +0300

    Update satori-todomvc

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ const PORT = process.env.PORT || 8085;
 const http = require('http');
 const nodeStatic = require('node-static');
 const SatoriServer = require('satori-server').SatoriServer;
-const todomvc = require('satori-todomvc/js/todomvc');
+const {TodosModel, Todo} = require('satori-todomvc/js/model');
+const {ViewModel} = require('satori-todomvc/js/viewmodel');
+const {TodoAppComponent} = require('satori-todomvc/js/view');
 const indexHtml = require('./index.html');
 
 let view = new SatoriServer();
-let model = new todomvc.TodosModel(require('./data.json').map(todo => new todomvc.Todo(todo)));
-let vm = new todomvc.ViewModel(model, view);
+let model = new TodosModel(require('./data.json').map(todo => new Todo(todo)));
+let vm = new ViewModel(model, view);
 let staticServer = new nodeStatic.Server('./node_modules');
 
 http.createServer(function (request, response) {
 	if (request.url === '/') {
-		response.end(indexHtml(todomvc.TodoAppComponent(vm, view)));
+		response.end(indexHtml(TodoAppComponent(vm, view)));
 		return;
 	}
 
